8664b8e98c1394314259a819d60d9a30e93a67ad
William Montgomery <william.montgomery@rigor.com>
2015-09-18 13:42:21 -0400

    Removed artificial sleep

--- server.rb
+++ server.rb
@@ -6,9 +6,6 @@ loop {                         # Servers run forever
   begin  
     client = ServerSingleton.server.accept       # Wait for a client to connect
 
-    # TODO remove this
-    sleep 1 # Wait to receive all of the message
-
     env = EnvironmentReader.new(client).to_h
     puts "#{Time.now.ctime} Received request #{env['REQUEST_METHOD']} #{env['_REQUEST_URL']}"
     puts "#{Time.now.ctime} #{env.inspect}"
