23b30f5a9a3d73a4b985825469b413486f939d4e
William Montgomery <william.montgomery@rigor.com>
2015-09-18 13:38:41 -0400

    Change status code of aborted to 0

--- app/aborted.rb
+++ app/aborted.rb
@@ -1,7 +1,7 @@
 class Aborted < Responder
   def call
     # Read the request
-    client.puts('HTTP/1.1 200 Aborted')
+    client.puts('HTTP/1.1 0 Aborted')
     client.puts()
     client.puts('Aborted')
   end
